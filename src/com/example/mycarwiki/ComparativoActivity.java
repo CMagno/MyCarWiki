abb52a8a724718281a9fcf50f12065771076a81a
CMagno <ca.magno00@gmail.com>
2014-01-30 18:23:56 -0300

    Comparativo.java dando erro

--- src/com/example/mycarwiki/ComparativoActivity.java
+++ src/com/example/mycarwiki/ComparativoActivity.java
@@ -3,13 +3,36 @@ package com.example.mycarwiki;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ComparativoActivity extends Activity {
-
+	
+	Spinner marcaCarro1, marcaCarro2, modeloCarro1, modeloCarro2;
+	ArrayAdapter<CharSequence> admarc1, admarc2, admod1, admod2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comparativo);
+		
+		/* Preenchimento dos Spinners */
+		marcaCarro1 = (Spinner) findViewById(R.id.spinnerMarcasCarro1Comparativo);
+		marcaCarro2 = (Spinner) findViewById(R.id.spinnerMarcasCarro2Comparativo);
+		modeloCarro1 = (Spinner) findViewById(R.id.spinnerModelosCarro1Comparativo);
+		modeloCarro2 = (Spinner) findViewById(R.id.spinnerModelosCarro2Comparativo);
+		admarc1 = ArrayAdapter.createFromResource(this, R.array.string_array_marcas_comparativo, android.R.layout.simple_spinner_item);
+		admarc2 = ArrayAdapter.createFromResource(this, R.array.string_array_marcas_comparativo, android.R.layout.simple_spinner_item);
+		admod1 = ArrayAdapter.createFromResource(this, R.array.string_array_modelos_chevrolet_comparativo, android.R.layout.simple_spinner_item);
+		admod1 = ArrayAdapter.createFromResource(this, R.array.string_array_modelos_chevrolet_comparativo, android.R.layout.simple_spinner_item);
+		admarc1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		admarc2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		admod1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		admod2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		marcaCarro1.setAdapter(admarc1);
+		marcaCarro2.setAdapter(admarc2);
+		modeloCarro1.setAdapter(admod1);
+		modeloCarro2.setAdapter(admod2);
 	}
 
 	@Override
